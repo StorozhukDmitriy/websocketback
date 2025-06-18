f3a2b640f4edf226c728b0cc758a39fc5a77a7c5
Dmitriy Storozhuk <gelendzhik94@yandex.ru>
2025-06-18 16:41:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting Started
+
+### 1. Install dependencies:
+
+```bash
+pnpm i
+```
+
+### 2. Start app:
+
+```bash
+pnpm run start-dev
+```
+
---
+++ src/app.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+
+const messages: any[] = [
+];
+
+const usersState = new Map();
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*" // Настройте правильно для продакшена!
+    }
+});
+
+app.get('/', (_req, res) => {
+    res.send('<h1>Socket.IO Server</h1>');
+});
+
+io.on('connection', (socketChannel) => {
+    console.log('New client connected:', socketChannel.id);
+    socketChannel.emit('init-messages-published', messages)
+
+    socketChannel.on('disconnect', () => {
+        usersState.delete(socketChannel);
+    });
+
+    usersState.set(socketChannel, {id: new Date().getTime().toString(), name: 'anonymous'});
+
+    socketChannel.on('client-name-sent', (name: string) => {
+        if (typeof name !== 'string') {
+            return;
+        }
+        const user = usersState.get(socketChannel);
+        user.name = name;
+    });
+    socketChannel.on('client-typed', () => {
+        io.emit('user-typing',usersState.get(socketChannel));
+    });
+
+    socketChannel.on('client-message-sent', (message: string) => {
+        if (typeof message !== 'string') {
+        return;
+        }
+        const user = usersState.get(socketChannel);
+        let newMessage = {
+            message: message, id: new Date().getTime(),
+            user: {id: user.id, name: user.name}};
+        messages.push(newMessage);
+        io.emit('new-message-sent', newMessage);
+    });
+
+});
+
+const PORT = process.env.PORT || 3009;
+httpServer.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
